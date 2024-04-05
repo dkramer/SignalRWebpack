99015135f7d53103bbe436693673782b0b077ef1
djkramer <kramerdakota@gmail.com>
2024-04-05 15:01:12 -0500

    add more

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+builder.Services.AddSignalR();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
